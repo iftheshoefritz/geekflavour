d5ee59d0b81c1e1c164b5db13f1213b07a55259e
Fritz Meissner <fritz.meissner@gmail.com>
2020-11-24 08:17:24 +0200

    Add error handling
    Add octokit initializer to raise errors when there is no http success
    
    Strategy:
    
    Continue if there is an error with one project
    Give feedback if there is an error outside the project loop

--- app/jobs/language_job.rb
+++ app/jobs/language_job.rb
@@ -5,32 +5,44 @@ class LanguageJob < ApplicationJob
   queue_as :default
 
   def perform(callback_code, session_id) # consider passing username
-    @session_id = session_id
-    status_update "Obtaining access token from Github..."
-    response = RestClient.post(
-      'https://github.com/login/oauth/access_token',
-      {
-        :client_id => client_id,
-        :client_secret => client_secret,
-        :code => callback_code
-      },
-      accept: :json
-    )
+    begin
+      @session_id = session_id
+      status_update "Obtaining access token from Github..."
+      response = RestClient.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          :client_id => client_id,
+          :client_secret => client_secret,
+          :code => callback_code
+        },
+        accept: :json
+      )
 
-    access_token = JSON.parse(response)['access_token']
-    client = Octokit::Client.new(access_token: access_token)
-    status_update "fetching User repos..."
-    repos = client.user[:repos_url]
-    project_languages_urls = client.get(repos).map(&:languages_url)
-    top_languages_per_repo = project_languages_urls.each_with_object([]) do |url, memo|
-      project = url.split("/").last(2).join("/")
-      status_update "fetching #{project}"
-      memo << client.get(url).sort_by { |_language, lines| lines }.last&.first
+      access_token = JSON.parse(response)['access_token']
+      client = Octokit::Client.new(access_token: access_token)
+      status_update "fetching User repos..."
+      repos = client.user[:repos_url]
+      project_languages_urls = client.get(repos).map(&:languages_url)
+
+      top_languages_per_repo = project_languages_urls.each_with_object([]) do |url, memo|
+        begin
+          project = url.split("/").last(2).join("/")
+          status_update "fetching #{project}"
+          memo << client.get(url).sort_by { |_language, lines| lines }.last&.first
+        rescue Octokit::Error => e
+          Rails.logger.error("Error fetching info at project URL #{url}")
+          log_error(e)
+        end
+      end
+      status_update "Counting use of languages in your repos..."
+      top_languages_count = top_languages_per_repo.each_with_object(Hash.new(0)) { |lg, result| result[lg] += 1 }
+      top_language = top_languages_count.max_by { |_lg, count| count }.first
+      answer_update(top_language.to_s)
+
+    rescue StandardError => e
+      status_update("Ack! Error fetching from github: #{e.message}")
+      log_error(e)
     end
-    status_update "Counting use of languages in your repos..."
-    top_languages_count = top_languages_per_repo.each_with_object(Hash.new(0)) { |lg, result| result[lg] += 1 }
-    top_language = top_languages_count.max_by { |_lg, count| count }.first
-    answer_update(top_language.to_s)
   end
 
   def client_id
@@ -67,5 +79,9 @@ class LanguageJob < ApplicationJob
       html: msg
     )
     cable_ready.broadcast
+  end
+
+  def log_error(error)
+    Rails.logger.error [error.message, *error.backtrace].join($/)
   end
 end
---
+++ config/initializers/octokit.rb
@@ -0,0 +1,9 @@
+stack = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::Request::Retry, exceptions: [Octokit::ServerError]
+  builder.use Octokit::Middleware::FollowRedirects
+  builder.use Octokit::Response::RaiseError
+  builder.use Octokit::Response::FeedParser
+  builder.response :logger
+  builder.adapter Faraday.default_adapter
+end
+Octokit.middleware = stack
