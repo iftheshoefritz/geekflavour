071a2f1d3fd57853d96f46d0e6eee955312a9652
Fritz Meissner <fritz.meissner@gmail.com>
2020-11-25 06:35:15 +0200

    Remove unused dependency

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,5 @@
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("channels");
 
 require("stylesheets/application.scss");
-
-const application = Application.start();
-const context = require.context("../controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
